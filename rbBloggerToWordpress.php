9cd1cbc1a89056de6496bead6b9b4b094827749a
rahul286 <rahul286@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-24 08:30:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/blogger-to-wordpress-redirection/trunk@35996 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rbBloggerToWordpress.php
+++ rbBloggerToWordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: Blogger To Wordpress Redirection
 Plugin URI: http://www.devilsworkshop.org/2008/01/19/blogger-to-wordpress-redirection-plugin-with-1-to-1-mapping-between-old-and-new-blog-posts/
 Description: If you have imported your blog from blogspot then you might be redirecting visitors from your old blogspot blog to your new wordpress blogs HOMEPAGE. This plugin detect all such redirections and find one-to-one mapping between blogspot post and wordpress post. Then it redirect users to the same post on new wordpress blog for which they actually came looking for!
-Version: 1.0 
+Version: 1.0
 Author: Rahul Bansal
 Author URI: http://www.devilsworkshop.org
 */
@@ -11,7 +11,7 @@ Author URI: http://www.devilsworkshop.org
 
 <?php
 /*
-Special Thanks to Charles [link: http://charles.gagalac.us/] 
+Special Thanks to Charles [link: http://charles.gagalac.us/]
 He helped while debugging code on #wordpress IRC channel.
 */
 ?>
@@ -25,22 +25,22 @@ DO NOT remove quotes i.e " and "
 
 $oldBlogURL = "rb286.blogspot.com";
 global $wpdb;
-//echo $oldBlogURL; 
+//echo $oldBlogURL;
 //following is to avoid infinite redirection problem
-if( strcmp( $_SERVER['REQUEST_URI'] , "/" ) == 0 ){
-   //"We are on Index Page";
+if( is_home() ){
+   //We are on Home Page
 	$ref = $_SERVER['HTTP_REFERER'];
 	$refarr = explode("/", $ref);
 
 	if ($refarr[2] == $oldBlogURL ){
 		$bloggerurl = '\/'.$refarr[3].'\/'.$refarr[4].'\/'.$refarr[5];
 		$sqlstr = "
-			    SELECT wposts.guid 
+			    SELECT wposts.guid
 			    FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
-			    WHERE wposts.ID = wpostmeta.post_id 
-			    AND wpostmeta.meta_key = 'blogger_permalink' 
+			    WHERE wposts.ID = wpostmeta.post_id
+			    AND wpostmeta.meta_key = 'blogger_permalink'
 			    AND wpostmeta.meta_value = '".$bloggerurl."'
-			 ";             
+			 ";
 		$wpurl = $wpdb->get_results($sqlstr, ARRAY_N);
 		if ($wpurl){
 			header( 'Location: '.$wpurl[0][0].' ') ;
@@ -52,5 +52,5 @@ if( strcmp( $_SERVER['REQUEST_URI'] , "/" ) == 0 ){
 ?>
 
 <?php
-add_action('wp_head','rbBloggerToWordpress');
+add_action('wp_head','rbBloggerToWordpress', '1');
 ?>
