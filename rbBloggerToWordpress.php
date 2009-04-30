08bdefae380889db50583c32f5bada74ba5d3b92
rahul286 <rahul286@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-30 19:04:42 +0000

    tagging version 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/blogger-to-wordpress-redirection/tags/1.1.2@114673 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rbBloggerToWordpress.php
+++ rbBloggerToWordpress.php
@@ -1,21 +1,13 @@
 <?php
 /*
 Plugin Name: Blogger To Wordpress Redirection
-Plugin URI: http://www.devilsworkshop.org/2008/01/19/blogger-to-wordpress-redirection-plugin-with-1-to-1-mapping-between-old-and-new-blog-posts/
+Plugin URI: http://www.devilsworkshop.org/blogger-to-wordpress-traffic-permalinks-redirection-plugin/
 Description: If you have imported your blog from blogspot then you might be redirecting visitors from your old blogspot blog to your new wordpress blogs HOMEPAGE. This plugin detect all such redirections and find one-to-one mapping between blogspot post and wordpress post. Then it redirect users to the same post on new wordpress blog for which they actually came looking for!
-Version: 1.1.1
+Version: 1.1.2
 Author: Rahul Bansal
 Author URI: http://www.devilsworkshop.org
 */
 ?>
-
-<?php
-/*
-Special Thanks to Charles [link: http://charles.gagalac.us/]
-He helped while debugging code on #wordpress IRC channel.
-*/
-?>
-
 <?php
 function rbBloggerToWordpress(){
 /*
@@ -50,7 +42,6 @@ if( strcmp( $_SERVER['REQUEST_URI'] , "/" ) == 0 ){
   }
 }
 ?>
-
 <?php
-add_action('init','rbBloggerToWordpress', '1');
+	add_action('init','rbBloggerToWordpress', '1');
 ?>
